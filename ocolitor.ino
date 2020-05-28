6fcb9dc6b2b94d6d5e2922cc70647cba280e0317
ddican <65253184+ddican@users.noreply.github.com>
2020-05-28 18:54:10 +0300

    Update ocolitor.ino

--- ocolitor.ino
+++ ocolitor.ino
@@ -1,176 +1,151 @@
+#define a1 4  
+#define b1 5 
+#define a2 6  
+#define b2 7 
 
-#include <Servo.h>          
-#include <NewPing.h>        
+int ledRosu=12;
+int ledAlbastru=13;
 
-//our L298N control pins
-const int a1 = 4;
-const int b1 = 5;
-const int a2 = 6;
-const int b2 = 7;
-int S1=0;
-int obstacol=0;
+int viteza1=0; 
+int viteza2=0; 
 
-int distantaDreapta = 0;
-int distantaStanga = 0;
+#define ir1 A0
+#define ir2 A1
+#define ir3 A2
+#define ir4 A3
+#define ir5 A4
 
-//sensor pins
-#define trig_pin 3 //analog input 1
-#define echo_pin 11 //analog input 2
-#define ir 2
-#define distanta_maxima 200
-int distanta = 100;
+void setup() {
 
-NewPing sonar(trig_pin, echo_pin, distanta_maxima); //sensor function
-Servo servo_motor; //our servo name
-
-
-void setup(){
+pinMode(ledRosu, OUTPUT);
+pinMode(ledAlbastru, OUTPUT);
+  
   pinMode(a1, OUTPUT);
   pinMode(b1, OUTPUT);
   pinMode(a2, OUTPUT);
-  pinMode(b2, OUTPUT);
-  pinMode(ir, INPUT);
+  pinMode(b1, OUTPUT);
   
-  servo_motor.attach(10); //our servo pin
-  servo_motor.write(115);
-  
-  delay(2000);
-  
-  distanta = readPing();
-  delay(100);
-  distanta = readPing();
-  delay(100);
-  distanta = readPing();
-  delay(100);
-  distanta = readPing();
-  delay(100);
+  pinMode(ir1, INPUT);
+  pinMode(ir2, INPUT);
+  pinMode(ir3, INPUT);
+  pinMode(ir4, INPUT);
+  pinMode(ir5, INPUT);
 }
 
-void loop(){
+void loop() {
+  int s1 = analogRead(ir1);  
+  int s2 = analogRead(ir2);  
+  int s3 = analogRead(ir3);  
+  int s4 = analogRead(ir4);  
+  int s5 = analogRead(ir5);  
+{
+  pinMode(ledRosu, HIGH);
+  delay(100);
+  pinMode(ledRosu, LOW);
+
+  pinMode(ledAlbastru,HIGH);
+  delay(100);
+  pinMode(ledAlbastru,LOW);
+
+}
+
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 1) && (s5 == 1))
   {
-  S1=digitalRead(ir);
-  
-  if(S1==LOW)
-  {
-    digitalWrite(obstacol,LOW);
-   inainteaza();
-    }
-  }
-
-if(S1==HIGH)
-  {
-    digitalWrite(obstacol,HIGH);
-   oprire();
-  }
-  delay(500);
-  inainteaza();
-  
-  
-   if (distanta <= 20){
-    oprire();
-    delay(300);
-    revers();
-    delay(400);
-    oprire();
-    delay(300);
-    distantaDreapta = privesteDreapta();
-    delay(300);
-    distantaStanga = privesteStanga();
-    delay(300);
-
-    if (distanta >= distantaStanga){
-      directieDrapta();
-      oprire();
-    }
-    else{
-      directieStanga();
-      oprire();
-    }
-  }
-  else{
-    inainteaza(); 
-  }
-    distanta = readPing();
-}
-
-int privesteDreapta(){  
-  servo_motor.write(50);
-  delay(500);
-  int distanta = readPing();
-  delay(100);
-  servo_motor.write(115);
-  return distanta;
-}
-
-int privesteStanga(){
-  servo_motor.write(170);
-  delay(500);
-  int distanta = readPing();
-  delay(100);
-  servo_motor.write(115);
-  return distanta;
-  delay(100);
-}
-
-int readPing(){
-  delay(70);
-  int cm = sonar.ping_cm();
-  if (cm==0){
-    cm=250;
-  }
-  return cm;
-}
-
-void oprire(){
-  
-  digitalWrite(a1, LOW);
-  digitalWrite(b1, LOW);
-  digitalWrite(a2, LOW);
-  digitalWrite(b2, LOW);
-}
-
-void inainteaza(){
-
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
     digitalWrite(a1, HIGH);
     digitalWrite(b1, LOW);
     digitalWrite(a2, HIGH);
     digitalWrite(b2, LOW);
   }
-
-
-void revers(){
   
-  digitalWrite(a1, LOW);
-  digitalWrite(b1, HIGH);
-  digitalWrite(a2, LOW);
-  digitalWrite(b2, HIGH);
-  
-}
-
-void directieDrapta(){
-
+  if((s1 == 1) && (s2 == 0) && (s3 == 1) && (s4 == 1) && (s5 == 1))
+  {
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
     digitalWrite(a1, HIGH);
     digitalWrite(b1, LOW);
     digitalWrite(a2, LOW);
     digitalWrite(b2, LOW);
+  }
   
-  delay(500);
-  
-  digitalWrite(a1, HIGH);
-  digitalWrite(b1, HIGH);
-  digitalWrite(a2, LOW);
-  digitalWrite(b2, LOW);
- 
- }
-void directieStanga(){
+  if((s1 == 0) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 1))
+  {
+    analogWrite(viteza1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(viteza2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(a1, HIGH);
+    digitalWrite(b1, LOW);
+    digitalWrite(a2, LOW);
+    digitalWrite(b2, HIGH);
+  }
 
+  if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 0) && (s5 == 1))
+  {
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
     digitalWrite(a1, LOW);
     digitalWrite(b1, LOW);
     digitalWrite(a2, HIGH);
     digitalWrite(b2, LOW);
+  }
 
-  delay(500);
-  
+  if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 0))
+  {
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
+    digitalWrite(a1, LOW);
+    digitalWrite(b1, HIGH);
+    digitalWrite(a2, HIGH);
+    digitalWrite(b2, LOW);
+  }
+
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 1))
+  {
+    //going left with full speed 
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
+    digitalWrite(a1, LOW);
+    digitalWrite(b1, LOW);
+    digitalWrite(a2, HIGH);
+    digitalWrite(b2, LOW);
+  }
+
+  if((s1 == 1) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
+    digitalWrite(a1, HIGH);
+    digitalWrite(b1, LOW);
+    digitalWrite(a2, LOW);
+    digitalWrite(b2, LOW);
+  }
+
+  if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    analogWrite(viteza1, 255);
+    analogWrite(viteza2, 255); 
+    digitalWrite(a1, HIGH);
+    digitalWrite(b2, LOW);
+    digitalWrite(a2, LOW);
+    digitalWrite(b2, LOW);
+  }
+
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 0))
+  {
+    //going left with full speed 
+    analogWrite(viteza1, 255); 
+    analogWrite(viteza2, 255); 
+    digitalWrite(a1, LOW);
+    digitalWrite(b1, LOW);
+    digitalWrite(a2, HIGH);
+    digitalWrite(b2, LOW);
+  }
+
+  if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 0) && (s5 == 0))
+  {
+    digitalWrite(a1, LOW);
+    digitalWrite(b1, LOW);
+    digitalWrite(a2, LOW);
+    digitalWrite(b2, LOW);
+  }
 }
-
- 
